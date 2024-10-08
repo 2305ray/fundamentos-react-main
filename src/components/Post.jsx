57534e0de3fcdbabb6f85dea209f825107cb9e8c
2305ray <rayssagarcia035@gmail.com>
2024-10-08 13:50:45 -0300

    post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,6 @@
+import { format, formatDistanceToNow } from 'date-fns'; 
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
@@ -6,24 +9,48 @@ import styles from './Post.module.css';
 
 
 
-export function Post(props) {
-    
+export function Post({author, publishedAt, content}) {
+    /* exemplo de uso de data pelo js
+    const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
+        day: '2-digit',
+        month:'long',
+        hour: '2-digit',
+        minute: '2-digit'
+    }).format(publishedAt);
+    */
+
+    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'ás' HH:mm", {
+        locale: ptBR, })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
     return(
         <article className={styles.post}> 
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/mayk.png" />
+                    <Avatar src={author.avatarUrl}/>
                     <div className={styles.authorinfo}>
-                        <strong>Rayssa Garcia</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-            <time title='04 de outubro de 2024' dateTime="2024-10-04 13:47:38">Publicado há 1h</time>
+            <time title='04 de outubro de 2024' dateTime={publishedAt.toISOString()}>
+               {publishedDateFormatted}
+                </time>
             </header>
 
             <div className={styles.content}>
-                
+                {content.map(line => {
+                    if (line.type == 'paragraph'){
+                        return <p>{line.content}</p>
+                    } else if (line.type == 'link') {
+                        return <p><a href=''>{line.content}</a></p>
+                    }
+                })}
             </div>    
 
             <form className={styles.commentForm}>
